c2c6118a4797dadfa651bf9e349323397369bbbb
suicidalTendencies <ytrewqqwerty1024@yandex.ru>
2025-03-27 22:51:00 +0300

    Запрет кеширования запроса инфы о люках

--- HatchMap.Server/Program.fs
+++ HatchMap.Server/Program.fs
@@ -1,17 +1,23 @@
 open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Hosting
 open Hatches
 
 [<EntryPoint>]
 let main args =
-    let builder = WebApplication.CreateBuilder(args)
+    let builder = WebApplication.CreateBuilder args
     let app = builder.Build()
 
     app.UseStaticFiles() |> ignore
     
-    app.MapGet("/hatches", Func<obj>(fun () -> getPhotoInfos)) |> ignore
-    app.MapFallbackToFile("index.html") |> ignore
+    app.MapGet("/hatches", Func<HttpContext, obj>(fun context -> 
+        //запрет кеширования
+        context.Response.Headers.CacheControl <- "no-store"
+        context.Response.Headers.Pragma <- "no-cache"
+        getPhotoInfos
+        )) |> ignore
+    app.MapFallbackToFile "index.html" |> ignore
     app.Run()
 
     0 // Exit code
